7c6602a01cb29369c83a6c1ba3d9061be8672253
foureverhh <foureverhh@gmail.com>
2018-12-11 00:14:11 +0100

    insert value to database

--- app/src/main/java/com/example/zfgg04/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/sqliteapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.zfgg04.sqliteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +23,20 @@ public class MainActivity extends AppCompatActivity {
         editMarks = findViewById(R.id.editText_marks);
         button = findViewById(R.id.button);
 
-        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean inInserted = myDb.insertDate(editName.getText().toString(),
+                        editSurname.getText().toString(),
+                        editMarks.getText().toString());
+                if(inInserted)
+                    Toast.makeText(MainActivity.this,
+                            "Data inserted",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,
+                            "Data not inserted",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
